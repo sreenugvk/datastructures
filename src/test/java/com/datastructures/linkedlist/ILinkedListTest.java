5b734a7ba48b7cebd28f0ee51ef5e710f9812d8c
Sreenivasulu Modem <sreenivasulu.modem@mutualofomaha.com>
2020-07-30 21:28:19 -0500

    added junit 5 dependencies and added test case for linked list

---
+++ src/test/java/com/datastructures/linkedlist/ILinkedListTest.java
@@ -0,0 +1,99 @@
+package com.datastructures.linkedlist;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ILinkedListTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testILinkedList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testInsertAtBegin() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testInsertAtEnd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testInsert() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRemoveMatched() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRemoveInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRemoveString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRemoveFirst() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRemoveLast() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetPosition() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testClearList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
